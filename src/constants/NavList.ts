a3a0caa8b01680f8c29f4558c4598f2fa2df0886
masud90895 <masudhossainmbs129@gmail.com>
2023-12-30 15:06:09 +0600

    Update styles and dependencies.also added navbar

---
+++ src/constants/NavList.ts
@@ -0,0 +1,19 @@
+import { FaHome } from "react-icons/fa";
+import { FaAngellist, FaTrophy } from "react-icons/fa6";
+export const NavList = [
+  {
+    name: "Home",
+    path: "/",
+    icon: FaHome,
+  },
+  {
+    name: "Quizzes",
+    path: "/quizzes",
+    icon: FaAngellist,
+  },
+  {
+    name: "Leaderboard",
+    path: "/leaderboard",
+    icon: FaTrophy,
+  },
+];
