38cdd0a28cbec265efa76dc8f012562a16abcf91
masud90895 <masudhossainmbs129@gmail.com>
2023-12-31 16:40:00 +0600

    Add admin dashboard link to Navbar

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/shared/Navbar/Navbar";
+import Footer from "@/components/shared/Footer/Footer";
+import Image from "next/image";
+import DashboardNavbar from "@/components/dashboard/DashboardNavbar/DashboardNavbar";
+import DashboardSidebar from "@/components/dashboard/DashboardSidebar/DashboardSidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dashboard-Quizzes",
+  description: "Quizzes is a quiz app built with Next.js and TypeScript.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="bg-slate-200 flex h-screen">
+          {/* <!-- /Sidebar --> */}
+          <DashboardSidebar />
+          <div className="flex h-full w-full flex-col">
+            {/* <!-- Navbar --> */}
+            <DashboardNavbar />
+            {/* <!-- /Navbar --> */}
+            {/* <!-- Main --> */}
+            <div className="h-full overflow-hidden p-3">
+              <main
+                id="dashboard-main"
+                className="h-[calc(100vh-10rem)] overflow-auto px-4 py-10 bg-white rounded-lg shadow-md"
+              >
+                {children}
+              </main>
+            </div>
+            {/* <!-- /Main --> */}
+           
+          </div>
+        </div>
+      </body>
+    </html>
+  );
+}
--- src/components/shared/Navbar/Navbar.tsx
+++ src/components/shared/Navbar/Navbar.tsx
@@ -114,6 +114,21 @@ const Navbar = () => {
                       leaveTo="transform opacity-0 scale-95"
                     >
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        {isLoggedIn?.role === "admin" && (
+                          <Menu.Item>
+                            {({ active }) => (
+                              <Link
+                                href="/dashboard"
+                                className={
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                }
+                              >
+                                Dashboard
+                              </Link>
+                            )}
+                          </Menu.Item>
+                        )}
+
                         <Menu.Item>
                           {({ active }) => (
                             <a
