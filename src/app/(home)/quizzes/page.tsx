bf883f1abe8a37477e8e974ed55f284e0ffc59d7
masud90895 <masudhossainmbs129@gmail.com>
2023-12-31 22:51:28 +0600

    Update grid layout in quizzes page

--- src/app/(home)/quizzes/page.tsx
+++ src/app/(home)/quizzes/page.tsx
@@ -31,7 +31,7 @@ const Quizzes = () => {
         </div>
       </div> */}
 
-      <section className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0 ">
+      <section className="grid grid-cols-1 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0 ">
         {data?.map((quiz: any, i: number) => (
           <QuizCard key={i} quiz={quiz} />
         ))}
