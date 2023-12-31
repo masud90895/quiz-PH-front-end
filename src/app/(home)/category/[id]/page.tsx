e4a9a0cd4d669668af41e8d15d0a006fc82a2bb3
masud90895 <masudhossainmbs129@gmail.com>
2023-12-31 22:32:57 +0600

    Remove commented out code in Quizzes component

---
+++ src/app/(home)/category/[id]/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+
+import QuizCard from "@/components/QuizCard/QuizCard";
+import Loader from "@/app/loading";
+import { useCategoryQuery } from "@/redux/api/categoryApi";
+
+const SingleCategory = ({ params }: { params: { id: string } }) => {
+  const { data, isLoading } = useCategoryQuery(params.id);
+
+  if (isLoading) return <Loader />;
+  return (
+    <div className="common">
+      <section className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0 ">
+        {data?.length > 0 ? (
+          data?.quiz?.map((quiz: any, i: number) => (
+            <QuizCard key={i} quiz={quiz} />
+          ))
+        ) : (
+          <h1 className="text-center text-2xl w-full">No quiz found</h1>
+        )}
+      </section>
+    </div>
+  );
+};
+
+export default SingleCategory;
--- src/app/(home)/quizzes/page.tsx
+++ src/app/(home)/quizzes/page.tsx
@@ -14,7 +14,7 @@ const Quizzes = () => {
 
   return (
     <div className="common">
-      <div
+      {/* <div
         className="relative h-56 rounded-b-lg bg-cover bg-center bg-no-repeat shadow-lg"
         style={{
           backgroundImage: `url(${CoverImage.src})`,
@@ -32,9 +32,9 @@ const Quizzes = () => {
             />
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <section className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0">
+      <section className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0 ">
         {data?.map((quiz: any, i: number) => (
           <QuizCard key={i} quiz={quiz} />
         ))}
