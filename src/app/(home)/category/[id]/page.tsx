1ec8460712c15a23a61e3e52814d9d82985032e6
masud90895 <masudhossainmbs129@gmail.com>
2024-01-01 11:56:11 +0600

    Add console log to SingleCategory component

--- src/app/(home)/category/[id]/page.tsx
+++ src/app/(home)/category/[id]/page.tsx
@@ -7,12 +7,13 @@ import { useCategoryQuery } from "@/redux/api/categoryApi";
 
 const SingleCategory = ({ params }: { params: { id: string } }) => {
   const { data, isLoading } = useCategoryQuery(params.id);
+  console.log("🚀 ~ file: page.tsx:10 ~ SingleCategory ~ data:", data);
 
   if (isLoading) return <Loader />;
   return (
     <div className="common">
       <section className="grid grid-cols-2 gap-x-6 gap-y-10 px-2 pb-20 sm:grid-cols-3 sm:px-8 lg:mt-16 lg:grid-cols-4 lg:gap-x-4 lg:px-0 ">
-        {data?.length > 0 ? (
+        {data?.quiz?.length > 0 ? (
           data?.quiz?.map((quiz: any, i: number) => (
             <QuizCard key={i} quiz={quiz} />
           ))
